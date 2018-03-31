a0ad7a9815c33fdffcfd0673ee09ccfff176166e
alexander.fournier <alexander.fournier@nasm.org>
2018-03-30 20:39:15 -0700

    first commit

---
+++ backend/src/main/java/com/alexanderfournier/site/demo/controllers/HomeController.java
@@ -0,0 +1,16 @@
+package com.alexanderfournier.site.demo.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index.html";
+	}
+	
+	
+
+}
